9fa8aab5fb40c6a929dc08293a4e413ef1ba6be4
kevyuu <kevin.kayu@gmail.com>
2023-07-18 16:18:32 +0700

    Fix warning on compiler.h

--- soul/src/core/compiler.h
+++ soul/src/core/compiler.h
@@ -128,7 +128,7 @@ namespace soul
     // an empty function body and the noreturn attribute.
 #ifdef __GNUC__ // GCC, Clang, ICC
     __builtin_unreachable();
-#elifdef _MSC_VER // MSVC
+#elif defined(_MSC_VER) // MSVC
     __assume(false);
 #endif
   }
