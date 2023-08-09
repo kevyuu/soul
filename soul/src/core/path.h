96fed208b73b01fda7874ee1d5b9002b49bb5434
kevyuu <kevin.kayu@gmail.com>
2023-08-09 10:49:17 +0700

    Implement path class

---
+++ soul/src/core/path.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <filesystem>
+#include "core/type_traits.h"
+#include <type_traits>
+
+namespace soul
+{
+  class Path : public std::filesystem::path
+  {
+  public:
+    constexpr Path(Path&& other) = default;
+    auto operator=(Path&& other) noexcept -> Path& = default;
+    constexpr ~Path() = default;
+
+    Path(std::filesystem::path&& path) // NOLINT
+        : std::filesystem::path(std::move(path))
+    {
+    }
+
+    [[nodiscard]]
+    static auto from(const char* str) -> Path
+    {
+      return std::filesystem::path(str);
+    }
+
+    [[nodiscard]]
+    auto clone() -> Path
+    {
+      return Path(*this);
+    }
+
+    auto clone_from(const Path& path) { *this = path; }
+
+  private:
+    Path(const Path& other) = default;
+    auto operator=(const Path& other) -> Path& = default;
+  };
+
+} // namespace soul
