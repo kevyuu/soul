577a9aa04d11fdde5eca121121e2e98ae14349b5
kevyuu <kevin.kayu@gmail.com>
2023-09-28 15:29:33 +0700

    Implement tuple

---
+++ soul/src/core/tuple.h
@@ -0,0 +1,255 @@
+#pragma once
+
+#include "core/builtins.h"
+#include "core/meta.h"
+#include "core/objops.h"
+#include "core/type_traits.h"
+
+namespace soul
+{
+  namespace impl
+  {
+    template <usize I, typename ValueT>
+    class TupleLeaf
+    {
+
+    public:
+      constexpr TupleLeaf() = default;
+
+      template <typename Arg>
+        requires(std::is_constructible_v<ValueT, Arg &&>)
+      constexpr explicit TupleLeaf(Arg&& arg) : value_(std::forward<Arg>(arg)) // NOLINT
+      {
+      }
+
+      [[nodiscard]]
+      constexpr auto clone() const -> TupleLeaf
+        requires(can_clone_v<ValueT>)
+      {
+        return TupleLeaf(value_.clone());
+      }
+
+      [[nodiscard]]
+      constexpr auto clone_from(const TupleLeaf& other)
+        requires(can_clone_v<ValueT>)
+      {
+        value_.clone_from(other.value_);
+      }
+
+      [[nodiscard]]
+      constexpr auto ref() -> ValueT&
+      {
+        return value_;
+      }
+
+      [[nodiscard]]
+      constexpr auto ref() const -> const ValueT&
+      {
+        return value_;
+      }
+
+    private:
+      ValueT value_;
+    };
+
+    template <usize IndexV, typename ValueT>
+      requires(std::is_empty_v<ValueT>)
+    class TupleLeaf<IndexV, ValueT> : public ValueT
+    {
+    public:
+      constexpr TupleLeaf() = default;
+
+      template <typename Arg>
+        requires(std::is_constructible_v<ValueT, Arg &&>)
+      constexpr explicit TupleLeaf(Arg&& arg) : ValueT(std::forward<Arg>(arg)) // NOLINT
+      {
+      }
+
+      [[nodiscard]]
+      constexpr auto clone() const -> TupleLeaf
+        requires(can_clone_v<ValueT>)
+      {
+        return TupleLeaf(ValueT::clone());
+      }
+
+      [[nodiscard]]
+      constexpr auto clone_from(const TupleLeaf& other)
+        requires(can_clone_v<ValueT>)
+      {
+        ValueT::clone_from(static_cast<const ValueT&>(other));
+      }
+
+      [[nodiscard]]
+      constexpr auto ref() -> ValueT&
+      {
+        return static_cast<ValueT&>(*this);
+      }
+
+      [[nodiscard]]
+      constexpr auto ref() const -> const ValueT&
+      {
+        return static_cast<const ValueT&>(*this);
+      }
+    };
+
+    template <typename IndexSequenceT, typename... Ts>
+    class TupleStorage;
+
+    template <usize... IndexVs, typename... Ts>
+    class TupleStorage<std::index_sequence<IndexVs...>, Ts...> : public TupleLeaf<IndexVs, Ts>...
+    {
+    public:
+      static constexpr b8 can_tuple_default_construct = (can_default_construct_v<Ts> && ...);
+      static constexpr b8 can_tuple_trivial_copy = (can_trivial_copy_v<Ts> && ...);
+      static constexpr b8 can_tuple_trivial_move = (can_trivial_move_v<Ts> && ...);
+      static constexpr b8 can_tuple_clone =
+        !can_tuple_trivial_copy && (can_copy_or_clone_v<Ts> && ...);
+      static constexpr b8 can_tuple_nontrivial_move =
+        !can_tuple_trivial_move && (can_move_v<Ts> && ...);
+      static constexpr b8 can_tuple_trivial_destruct = (can_trivial_destruct_v<Ts> && ...);
+      static constexpr b8 can_tuple_nontrivial_destruct = (can_nontrivial_destruct_v<Ts> || ...);
+      static constexpr b8 can_tuple_swap = (can_swap_v<Ts> && ...);
+
+      constexpr TupleStorage() = default;
+
+      template <typename... ValueTs>
+      constexpr explicit TupleStorage(
+        std::index_sequence<IndexVs...> /*indexes*/, ValueTs&&... values)
+          : TupleLeaf<IndexVs, Ts>(std::forward<ValueTs>(values))...
+      {
+      }
+
+      constexpr TupleStorage(const TupleStorage& other)
+        requires(can_tuple_trivial_copy)
+      = default;
+
+      constexpr TupleStorage(TupleStorage&& other) noexcept = default;
+
+      constexpr auto operator=(const TupleStorage& other) -> TupleStorage&
+        requires(can_tuple_trivial_copy)
+      = default;
+
+      constexpr auto operator=(TupleStorage&& other) noexcept -> TupleStorage& = default;
+
+      constexpr ~TupleStorage() = default;
+
+      [[nodiscard]]
+      constexpr auto clone() const -> TupleStorage
+        requires(can_tuple_clone)
+      {
+        return TupleStorage(
+          std::make_index_sequence<sizeof...(Ts)>(), duplicate(ref<IndexVs>())...);
+      }
+
+      constexpr void clone_from(const TupleStorage& other)
+        requires(can_tuple_clone)
+      {
+        (duplicate_from(
+           NotNull(static_cast<TupleLeaf<IndexVs, Ts>*>(this)),
+           static_cast<const TupleLeaf<IndexVs, Ts>&>(other)),
+         ...);
+      }
+
+      template <usize IndexV>
+      [[nodiscard]]
+      constexpr auto ref() const -> const get_type_at_t<IndexV, Ts...>&
+      {
+        using BaseT = const TupleLeaf<IndexV, get_type_at_t<IndexV, Ts...>>*;
+        BaseT base = this;
+        return base->ref();
+      }
+
+      template <usize IndexV>
+      [[nodiscard]]
+      constexpr auto ref() -> get_type_at_t<IndexV, Ts...>&
+      {
+        using BaseT = TupleLeaf<IndexV, get_type_at_t<IndexV, Ts...>>*;
+        BaseT base = this;
+        return base->ref();
+      }
+
+      friend constexpr void swap(TupleStorage& lhs, TupleStorage& rhs) noexcept
+        requires(can_tuple_swap)
+      {
+        using std::swap;
+        (swap(lhs.ref<IndexVs>(), rhs.ref<IndexVs>()), ...);
+      }
+
+      [[nodiscard]]
+      friend constexpr auto
+      operator==(const TupleStorage& lhs, const TupleStorage& rhs) -> b8
+        requires(conjunction_v<can_compare_equality_v<Ts>...>)
+      {
+        return ((lhs.ref<IndexVs>() == rhs.ref<IndexVs>()) && ...);
+      }
+    };
+  }; // namespace impl
+
+  template <typename T, typename... Ts>
+  class Tuple
+  {
+  private:
+    using storage = impl::TupleStorage<std::make_index_sequence<sizeof...(Ts) + 1>, T, Ts...>;
+    storage storage_;
+
+  public:
+    constexpr Tuple() = default;
+
+    template <typename Arg, typename... Args>
+    constexpr Tuple(Arg&& arg, Args&&... args) // NOLINT
+      requires(can_convert_v<Arg, T> && (can_convert_v<Args, Ts> && ...))
+        : storage_(
+            std::make_index_sequence<sizeof...(Ts) + 1>(),
+            std::forward<Arg>(arg),
+            std::forward<Args>(args)...)
+    {
+    }
+
+    template <usize IndexV>
+    [[nodiscard]]
+    constexpr auto ref() const -> const get_type_at_t<IndexV, T, Ts...>&
+    {
+      return storage_.template ref<IndexV>();
+    }
+
+    template <usize IndexV>
+    [[nodiscard]]
+    constexpr auto ref() -> get_type_at_t<IndexV, T, Ts...>&
+    {
+      return storage_.template ref<IndexV>();
+    }
+
+    [[nodiscard]]
+    constexpr auto clone() const -> Tuple
+      requires(can_clone_v<storage>)
+    {
+      return Tuple(storage_.clone());
+    }
+
+    constexpr void clone_from(const Tuple& other)
+      requires(can_clone_v<storage>)
+    {
+      storage_.clone_from(other.storage_);
+    }
+
+    friend constexpr void swap(Tuple& lhs, Tuple& rhs) noexcept
+      requires can_swap_v<storage>
+    {
+      return swap(lhs.storage_, rhs.storage_);
+    }
+
+    [[nodiscard]]
+    friend constexpr auto
+    operator==(const Tuple& lhs, const Tuple& rhs) -> b8
+      requires(can_compare_equality_v<storage>)
+    {
+      return lhs.storage_ == rhs.storage_;
+    }
+
+  private:
+    constexpr explicit Tuple(storage&& other_storage) : storage_(std::move(other_storage)) {}
+  };
+
+  template <class... ValueTs>
+  Tuple(ValueTs...) -> Tuple<ValueTs...>;
+} // namespace soul
---
+++ test/test_tuple.cpp
@@ -0,0 +1,173 @@
+#include <gtest/gtest.h>
+
+#include "core/config.h"
+#include "core/meta.h"
+#include "core/objops.h"
+#include "core/panic.h"
+#include "core/tuple.h"
+#include "core/type_traits.h"
+#include "core/views.h"
+#include "memory/allocator.h"
+
+#include "common_test.h"
+#include "util.h"
+
+namespace soul
+{
+  auto get_default_allocator() -> memory::Allocator*
+  {
+    static TestAllocator test_allocator("Test default allocator");
+    return &test_allocator;
+  }
+} // namespace soul
+
+struct TrivialObj {
+  u8 x;
+  u8 y;
+
+  friend constexpr auto operator<=>(const TrivialObj& lhs, const TrivialObj& rhs) = default;
+};
+
+struct MoveOnlyObj {
+  u8 x;
+  u8 y;
+
+  MoveOnlyObj(const MoveOnlyObj& other) = delete;
+  MoveOnlyObj(MoveOnlyObj&& other) noexcept : x(other.x), y(other.y) {}
+  auto operator=(const MoveOnlyObj& other) = delete;
+  auto operator=(MoveOnlyObj&& other) noexcept -> MoveOnlyObj&
+  {
+    x = other.x;
+    y = other.y;
+    return *this;
+  }
+
+  ~MoveOnlyObj()
+  {
+    x = 0;
+    y = 0;
+  }
+};
+
+using TrivialTuple = soul::Tuple<u8, u16, TrivialObj>;
+static_assert(soul::ts_copy<TrivialTuple>);
+static_assert(
+  soul::can_default_construct_v<u8> && soul::can_default_construct_v<u16> &&
+  soul::can_default_construct_v<TrivialObj>);
+
+using ListTestObject = soul::Vector<TestObject>;
+using NontrivialTuple = soul::Tuple<ListTestObject, TestObject, u8>;
+static_assert(soul::ts_clone<NontrivialTuple>);
+
+using MoveOnlyTuple = soul::Tuple<u8, MoveOnlyObj>;
+static_assert(soul::ts_move_only<MoveOnlyObj>);
+
+struct EmptyObj {
+};
+static_assert(sizeof(soul::Tuple<u8, EmptyObj>) == sizeof(u8));
+#include <utility>
+
+template <typename TupleT>
+auto verify_equal(const TupleT& lhs, const TupleT& rhs)
+{
+  SOUL_TEST_ASSERT_EQ(lhs, rhs);
+}
+
+TEST(TestTupleConstruction, TestConstructionDefault)
+{
+  TrivialTuple trivial_tuple;
+
+  NontrivialTuple nontrivial_tuple;
+  SOUL_TEST_ASSERT_EQ(nontrivial_tuple.ref<0>(), ListTestObject());
+  SOUL_TEST_ASSERT_EQ(nontrivial_tuple.ref<1>(), TestObject());
+}
+
+TEST(TestTupleConstruction, TestConstructionFromMember)
+{
+  {
+    const auto trivial_obj = TrivialObj{3, 4};
+    const auto trivial_tuple = TrivialTuple{1, 2, trivial_obj};
+    SOUL_TEST_ASSERT_EQ(trivial_tuple.ref<0>(), 1);
+    SOUL_TEST_ASSERT_EQ(trivial_tuple.ref<1>(), 2);
+    SOUL_TEST_ASSERT_EQ(trivial_tuple.ref<2>(), trivial_obj);
+  }
+
+  {
+    const auto test_list = ListTestObject::From(
+      std::views::iota(0, 10) | std::views::transform([](int i) { return TestObject(i); }));
+
+    NontrivialTuple nontrivial_tuple = {test_list.clone(), TestObject(3), 5};
+    SOUL_TEST_ASSERT_EQ(nontrivial_tuple.ref<0>(), test_list);
+    SOUL_TEST_ASSERT_EQ(nontrivial_tuple.ref<1>(), TestObject(3));
+    SOUL_TEST_ASSERT_EQ(nontrivial_tuple.ref<2>(), 5);
+  }
+
+  {
+    const auto test_tuple = soul::Tuple{8, EmptyObj{}};
+  }
+}
+
+TEST(TestTupleConstruction, TestCopyConstructor)
+{
+  SOUL_TEST_RUN(test_copy_constructor(TrivialTuple{1, 2, TrivialObj{3, 4}}));
+}
+
+TEST(TestTupleConstruction, TestClone)
+{
+  const auto test_list = ListTestObject::From(
+    std::views::iota(0, 10) | std::views::transform([](int i) { return TestObject(i); }));
+
+  NontrivialTuple nontrivial_tuple = {test_list.clone(), TestObject(3), 5};
+  SOUL_TEST_RUN(test_clone(nontrivial_tuple));
+}
+
+TEST(TestTupleConstruction, TestMoveConstructor)
+{
+
+  SOUL_TEST_RUN(test_move_constructor(TrivialTuple{1, 2, TrivialObj{3, 4}}));
+
+  const auto test_list = ListTestObject::From(
+    std::views::iota(0, 10) | std::views::transform([](int i) { return TestObject(i); }));
+  NontrivialTuple nontrivial_tuple = {test_list.clone(), TestObject(3), 5};
+  SOUL_TEST_RUN(test_move_constructor(nontrivial_tuple));
+}
+
+class TestTupleManipulation : public testing::Test
+{
+public:
+  TrivialTuple trivial_tuple = TrivialTuple{1, 2, TrivialObj{3, 4}};
+  TrivialTuple trivial_tuple2 = TrivialTuple{5, 6, TrivialObj{7, 8}};
+
+  TestObject test_obj = TestObject(10);
+  TestObject test_obj2 = TestObject(7);
+
+  ListTestObject list_test_obj = ListTestObject::From(
+    std::views::iota(3, 10) | std::views::transform([](i32 val) { return TestObject(val); }));
+  ListTestObject list_test_obj2 = ListTestObject::From(
+    std::views::iota(3, 7) | std::views::transform([](i32 val) { return TestObject(val); }));
+
+  NontrivialTuple nontrivial_tuple = {list_test_obj.clone(), test_obj.clone(), 1};
+  NontrivialTuple nontrivial_tuple2 = {list_test_obj2.clone(), test_obj2.clone(), 2};
+};
+
+TEST_F(TestTupleManipulation, TestCopyAssignment)
+{
+  SOUL_TEST_RUN(test_copy_assignment(trivial_tuple, trivial_tuple2));
+}
+
+TEST_F(TestTupleManipulation, TestMoveAssignment)
+{
+  SOUL_TEST_RUN(test_move_assignment(trivial_tuple, trivial_tuple2));
+  SOUL_TEST_RUN(test_move_assignment(nontrivial_tuple, nontrivial_tuple2));
+}
+
+TEST_F(TestTupleManipulation, TestCloneFrom)
+{
+  SOUL_TEST_RUN(test_clone_from(nontrivial_tuple, nontrivial_tuple2));
+}
+
+TEST_F(TestTupleManipulation, TestSwap)
+{
+  SOUL_TEST_RUN(test_swap(trivial_tuple, trivial_tuple2));
+  SOUL_TEST_RUN(test_swap(nontrivial_tuple, nontrivial_tuple2));
+}
